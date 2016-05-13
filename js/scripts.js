3db3bc7bdeabe87d9b4af12bb87563a6294986a1
Anthony Raffini <traffini@gmail.com>
2016-05-13 09:14:59 -0700

    add prefixer and clean code

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
-// My Scripts
+//my scripts
 
-// Store image URL's
+//store images
 var img1 = 'http://rottingroom.github.io/pitch/img/img1.jpg';
 var img2 = 'http://rottingroom.github.io/pitch/img/img2.jpg';
 var img3 = 'http://rottingroom.github.io/pitch/img/img3.jpg';
 var img4 = 'http://rottingroom.github.io/pitch/img/img4.jpg';
 var img5 = 'http://rottingroom.github.io/pitch/img/img5.jpg';
 
-// Get random number
+//get random number
 var ranNum = Math.floor((Math.random() * 5) + 1);
 
-// Put images on page
+//put images on page
 document.getElementById('background').style.backgroundImage = 'url("http://rottingroom.github.io/pitch/img/img' + ranNum + '.jpg")';
 
+//lettering plugin
 $('#myname').lettering();
